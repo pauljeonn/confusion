5b1a875793f81c84158fce6a8f6df60f950748a6
paul <pauljeonn@gmail.com>
2019-09-20 00:34:31 +0900

    react router

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,84 +1,72 @@
-import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle
-} from "reactstrap";
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-class Dishdetail extends Component {
-  constructor(props) {
-    super(props);
+// Q. Implement a function named renderDish() that takes the dish as a parameter and returns the JSX code for laying out the details of the dish in a reactstrap Card
 
-    this.state = {
-      selectedDish: null
-    };
+// This is the fuction for rendering the selected dish (received as an argument) as another card at the end
+const RenderDish = ({ dish }) => {
+  // Checkes if dish is really selected
+  if (dish != null)
+    return (
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  else return <div></div>;
+};
+
+// Q. Implement a function named renderComments() that takes the comments array as a parameter and lays out each comment as shown in the image below. You should use the Bootstrap list-unstyled class on the list.
+const RenderComments = ({ dish }) => {
+  if (dish != null) {
+    return (
+      <div>
+        <h4>Comments</h4>
+        <ul className="list-unstyled">
+          {dish.comments.map(comment => {
+            return (
+              <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>
+                  -- {comment.author} ,{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit"
+                  }).format(new Date(Date.parse(comment.date)))}{" "}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return <div></div>;
   }
+};
 
-  // Q. Implement a function named renderDish() that takes the dish as a parameter and returns the JSX code for laying out the details of the dish in a reactstrap Card
-
-  // This is the fuction for rendering the selected dish (received as an argument) as another card at the end
-  renderDish(dish) {
-    // Checkes if dish is really selected
-    if (dish != null)
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else return <div></div>;
-  }
-
-  // Q. Implement a function named renderComments() that takes the comments array as a parameter and lays out each comment as shown in the image below. You should use the Bootstrap list-unstyled class on the list.
-  renderComments(dish) {
-    if (dish != null) {
-      return (
-        <div>
-          <h4>Comments</h4>
-          <ul className="list-unstyled">
-            {dish.comments.map(comment => {
-              return (
-                <li key={comment.id}>
-                  <p>{comment.comment}</p>
-                  <p>
-                    -- {comment.author} ,{" "}
-                    {new Intl.DateTimeFormat("en-US", {
-                      year: "numeric",
-                      month: "short",
-                      day: "2-digit"
-                    }).format(new Date(Date.parse(comment.date)))}{" "}
-                  </p>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
-
-  render() {
+const Dishdetail = (props) => {
+  if (props.dish != null) {
     return (
       <div class="container">
         <div className="row">
           <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.props.dish)}
+            <RenderDish dish={props.dish} />
           </div>
           <div className="col-12 col-md-5 m-1">
-            {this.renderComments(this.props.dish)}
-          </div>
+          <RenderComments dish={props.dish} />
+          </div> 
         </div>
       </div>
     );
+  } else {
+    return (<div></div>)
   }
-}
+  
+};
 
 export default Dishdetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,72 +1,34 @@
-import React, { Component } from "react";
-import { Media } from "reactstrap";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle
-} from "reactstrap";
-import Dishdetail from "./DishdetailComponent";
+import React from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-class Menu extends Component {
-  constructor(props) {
-    super(props);
+function RenderMenuItem ({dish, onClick}) {
+  return (
+      <Card onClick={() => dish.id}>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+          </CardImgOverlay>
+      </Card>
+  );
+}
 
-    this.state = {
-      selectedDish: null
-    };
-  }
+function Menu(props) {
 
-  onDishSelect(dish) {
-    this.setState({ selectedDish: dish });
-  }
-
-  /*
-  // This is the fuction for rendering the selected dish (received as an argument) as another card at the end
-  renderDish(dish) {
-    // Checkes if dish is really selected
-    if (dish != null)
+  const menu = props.dishes.map((dish) => {
       return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
+          <div className="col-12 col-md-5 m-1"  key={dish.id}>
+              <RenderMenuItem dish={dish} onClick={props.onClick} />
+          </div>
       );
-    else return <div></div>;
-  }
-  */
+  });
 
-  // This renders all the cards in dishes
-  render() {
-    const menu = this.props.dishes.map(dish => {
-      return (
-        <div className="col-12 col-md-5 m-1">
-            <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
-              {/* Here, the component's state changes by setState and React decides to re-render the component. */}
-              <CardImg width="100%" src={dish.image} alt={dish.name} />
-              <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-              </CardImgOverlay>
-            </Card>
-        </div>
-      );
-    });
-
-    return (
+  return (
       <div className="container">
-        <div className="row">{menu}</div>
-        {/* 
-          This renders the selected dish at the end 
-          {this.renderDish(this.state.selectedDish)}
-        */}
+          <div className="row">
+              {menu}
+          </div>
       </div>
-    );
-  }
+  );
 }
 
 export default Menu;
